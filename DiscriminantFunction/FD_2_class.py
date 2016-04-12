dfef714dae3a95909a5e25665fcbd2696c1e2d16
syoamakase <syoamakase@gmail.com>
2016-04-12 16:12:07 +0900

    modified FD_2_class.py

--- DiscriminantFunction/FD_2_class.py
+++ DiscriminantFunction/FD_2_class.py
@@ -6,23 +6,28 @@ import copy
 import matplotlib.pyplot as plt
 from matplotlib import lines
 
-row = 0.03
+# to define ρ in w' = w + ρ*x
+ROW = 0.03
+# How many times the learning do?
+EPOCH = 15
+
+CLASS_1_MISS = 1
+CLASS_2_MISS = 2
+CORRECT      = 0
+
 #Discriminant function rule
 class DF():
 	#define k-NN(default is 1-NN)
-	def __init__(self,kNN,data,class_data):
-		self.kNN           = kNN
+	def __init__(self,data,class_data):
 		self.data          = data
 		self.class_data    = class_data
-		self.weight        = np.ones((2),dtype=np.float32)
-		with open('data.json','w') as f:
-			pass
-
+		#initialize weight
+		self.weight        = np.array((1,1),dtype=np.float32)
 
 	#calclate distance
 	## g(x) = g_1(x) - g_2(x)
 	def g_x(self,input_data):
-		##g(x) = wx + w_0
+		##g(x) = wx + w0
 		##if g(x) > 0 then x is class 1
 		##if g(x) < 0 then x is class 2
 		judge =  (self.weight*input_data).sum()
@@ -30,24 +35,24 @@ class DF():
 		return judge
 	
 	def error_correction(self,error_id,input_data):
-		global row
-		if error_id == 1:
-			self.weight += row*input_data
-		elif error_id == 2:
-			self.weight -= row*input_data
+		global ROW
+		if error_id == CLASS_1_MISS:
+			self.weight += ROW*input_data
+		elif error_id == CLASS_2_MISS:
+			self.weight -= ROW*input_data
 
 	def error_judgement(self,judge,class_data):
 		if judge <= 0 and class_data == 1:
-			return 1
-		elif judge >= 0 and class_data ==2:
-			return 2
+			return CLASS_1_MISS
+		elif judge >= 0 and class_data == 2:
+			return CLASS_2_MISS
 		else:
-			return 0
+			return CORRECT
 
 	def learning_loop(self,data,class_data):
 		prev_weight = copy.deepcopy(self.weight)
 		graph = graph_generator(1,self.weight)
-		for loop in range(15):
+		for loop in range(EPOCH):
 			for i in range(len(data)):
 				judge =  self.g_x(data[i])
 				error_id = self.error_judgement(judge,class_data[i])
@@ -86,5 +91,6 @@ if __name__== "__main__":
 	print "dataload"
 	fope = file_operator("2_class.data")
 	data,class_data = fope.getData()
-	df = DF(1,data,class_data)
+	print "learning start"
+	df = DF(data,class_data)
 	df.learning_loop(data,class_data)
