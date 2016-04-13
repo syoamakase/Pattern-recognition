ab129f70e88411cb163d5302fb1bd1993cf851d6
syoamakase <syoamakase@gmail.com>
2016-04-13 19:27:24 +0900

    add WH

--- Widrow-Hoff/WH.py
+++ Widrow-Hoff/WH.py
@@ -12,9 +12,22 @@ class WH():
 		self.weight = np.ones((c,len(data)),dtype=np.float32)
 		self.__data   = data		
 
-
 	def g_x(self,data,p):
 		gxp = (self.weight[p]*data[p]).sum()
 
 		return gxp
-		
+
+	def steepest_descent(self,p):
+		pass
+
+
+class file_operator():
+	def __init__(self,filename):
+		data = np.genfromtxt(filename,delimiter=",",dtype=np.float32,usecol(0)
+
+	def getData(self):
+		pass
+
+
+if __name__ == "__main__":
+			
