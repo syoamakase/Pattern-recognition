b1bd410e6e57a9b25ecbd4b4bd9d4a854f423d7f
syoamakase <syoamakase@gmail.com>
2016-04-17 20:51:12 +0900

    add class data

--- Widrow-Hoff/WH.py
+++ Widrow-Hoff/WH.py
@@ -7,28 +7,56 @@ import copy
 # ρ
 ROW = 0.03
 
-class WH():
-	def __init__(self,data,c):
-		self.weight = np.ones((c,len(data)),dtype=np.float32)
-		self.__data   = data		
+EPOCH = 15
 
-	def g_x(self,data,p):
-		gxp = (self.weight[p]*data[p]).sum()
+class WH():
+	def __init__(self,data,class_data):
+		self.weight      = np.ones((len(data),len(data[0])),dtype=np.float32)
+		self.data        = data
+		self.class_data  = class_data
+
+	# to calculate g(x) = wx
+	def g_x(self,p):
+		gxp = self.weight[p]*self.data[p]
 
 		return gxp
 
 	def steepest_descent(self,p):
 		pass
+	
+	# sum_of_squares 
+	def loss(self,input_data,superviser):
+		loss = ((input_data - superviser).sum())/2
+
+		return loss
+
+	# differentiate loss
+	# ∂J/∂w
+	def differential_calculus(self,input_data,superviser):
+		differentiate_loss = (g_x(input_data) - superviser)*input_data
+		
+		return differentiate_loss
+
+	def error_correction(self,p,differentiate_loss,input_data):
+		# w' = w - ρεx
+		self.weight[p] = self.weight[p] - ROW*differentiate_loss
+
+	def learning_loop(self,data,class_data):
+		
+		for loop in range(EPOCH):
+			for i in range(len(data)):
+				
 
 
 class file_operator():
 	def __init__(self,filename):
-		self.__data        = np.genfromtxt(filename,delimiter=",",dtype=np.float32,usecol=(0))
-		self.__class_data  = np.genfromtxt(filename,delimeter=",",dtype=np.float32,usecol=(1))
+		self.__data        = np.loadtxt(filename,delimiter=",",usecol=(0,1,2))
+		self.__class_data  = np.genfromtxt(filename,delimeter=",",dtype=np.float32,usecol=(3))
 
 	def getData(self):
-		pass
-
+		return self.__data, self.__class_data
 
 if __name__ == "__main__":
-			
+	fo = file_operator("class.data")
+	data,class_data = fo.getData()
+	wh = WH(data,len(data),class_data)
