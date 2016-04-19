3c6717ceed4ee0200eb95d19dc54fb32a9066585
syoamakase <syoamakase@gmail.com>
2016-04-19 16:13:44 +0900

    modified algorithm

--- Widrow-Hoff/WH.py
+++ Widrow-Hoff/WH.py
@@ -3,39 +3,47 @@
 
 import numpy as np
 
-# ρ
-ROW = 0.001 
+## ρ
+ROW = 0.2 
 
-# max learning epoch
-EPOCH = 100 
+## max learning epoch
+EPOCH = 1
 
 class WH():
-	# c means weight length
+	## c means weight length
 	def __init__(self,c,data_length):
-		self.weight      = np.ones((c,data_length),dtype=np.float32)
+		self.weight      = np.random.rand(c,data_length)
 
-	# to calculate g(x) = wx
+	## to calculate g(x) = wx
 	def g_x(self,i,p,input_data):
-		gxp = (self.weight[i]*input_data[p])
+		gxp = (self.weight[i]*input_data[p]).sum()
 
 		return gxp
 
-	# sum_of_squares 
-	def loss(self,i,p,input_data,superviser):
+	## sum_of_squares 
+	def loss(self,p,input_data,superviser):
+		
 		loss = (((self.g_x(i,p,input_data) - superviser[p])**2).sum())/2
 
 		return loss
 
-	# differentiate loss
-	# ∂J/∂w
-	def differential_calculus(self,i,p,input_data,superviser):
-		differentiate_loss = (self.g_x(i,p,input_data) - superviser[p])*input_data
-		
+	## differentiate loss
+	## ∂J/∂w
+	def differential_calculus(self,p,input_data,class_data):
+		superviser = np.zeros((len(self.weight)),dtype=np.int32)
+		differentiate_loss = np.zeros((len(self.weight),len(self.weight[0])),dtype=np.float32)
+		superviser[class_data[p]-1] = 1 
+		print(input_data[p])
+		for i in range(len(self.weight)):
+			differentiate_loss[i] = ((self.g_x(i,p,input_data) - superviser[i])*input_data[p])
+	
 		return differentiate_loss
 
-	def error_correction(self,i,p,differentiate_loss,input_data):
-		# w' = w - ρεx
-		self.weight[i] = self.weight[i] - (ROW*differentiate_loss[p])
+	def error_correction(self,p,differentiate_loss,input_data):
+		## w' = w - ρεx
+		for i in range(len(self.weight)):
+			self.weight[i] = self.weight[i] - (ROW*differentiate_loss[i])
+
 
 	# to start learning
 	def learning_loop(self,data,class_data):
@@ -43,31 +51,26 @@ class WH():
 		for loop in range(EPOCH):
 			print("epoch : {}".format(loop))
 			loss_all = 0
+			## data loop
 			for p in range(len(data)):
-				max_arg = 0
-				tmp = float("inf")
-				for i in range(len(self.weight)):
-					loss_all += self.loss(i,p,data,class_data)
-					differentiate_loss = self.differential_calculus(i,p,data,class_data)
-					print self.g_x(i,p,data)
-					if tmp > self.g_x(i,p,data):
-						arg_max = i+1
-						tmp = self.g_x(i,p,data)
-				if arg_max != class_data[p]:
-					self.error_correction(class_data[p]-1,p,differentiate_loss,data)
-					#print(self.g_x(i,p,data))
-			#print(self.weight)
-		for p in range(len(data)):
-			print("input : {}".format(data[p]))
-			for i in range(len(self.weight)):
-				print("g_{}_{} : {}".format(i,p,self.g_x(i,p,data))) 			
+				## g(x) loop
+				differentiate_loss = self.differential_calculus(p,data,class_data)
+				self.error_correction(p,differentiate_loss,data)
+		
+		for d in range(len(data)): 	
+			print("data: {} class : {} ".format(d+1,class_data[d]))
+			print(self.g_x(0,d,data))
+			print(self.g_x(1,d,data))
+			print(self.g_x(2,d,data))	
+		print(self.weight)
 
 
 class file_operator():
 	def __init__(self,filename):
 		data        = np.genfromtxt(filename,delimiter=",",dtype=np.float32,usecols=(0))
 		self.__data = np.c_[np.ones(len(data),dtype=np.float32),data]
-		self.__class_data  = np.genfromtxt(filename,delimiter=",",dtype=np.float32,usecols=(1))
+		self.__class_data  = np.genfromtxt(filename,delimiter=",",dtype=np.int32,usecols=(1))
+
 
 	def getData(self):
 		return self.__data, self.__class_data
@@ -75,5 +78,6 @@ class file_operator():
 if __name__ == "__main__":
 	fo = file_operator("class.data")
 	data,class_data = fo.getData()
+	print len(data[0])
 	wh = WH(3,len(data[0]))
 	wh.learning_loop(data,class_data)
