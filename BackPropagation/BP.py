fb140e93c06abe97d7d47522ff08749ee1c424af
syoamakase <syoamakase@gmail.com>
2016-04-25 19:40:50 +0900

    add gjp

--- BackPropagation/BP.py
+++ BackPropagation/BP.py
@@ -5,12 +5,19 @@ import numpy as np
 
 # to define network architecture
 # 
-NetWorks = [3,2,3,3]
+NetWorks = {
+    'num_of_layer'  : 3,
+    'input_layer'   : 2,
+    'hidden1_layer' : 4,
+    'output_layer'  : 3
+}
+
+FILENAME="class.data"
 
 class BP():
     def __init__(self,Net):
         pass
-    
+
     # output unit j for input i
     def h_j_p(self,j,p,input_data):
         pass
@@ -20,12 +27,15 @@ class BP():
         return 1/(1+np.exp(-1*gjp))
 
     # activationg function - tanhx
-    def tanh(self,gjp):
+    def tanh(self,hjp):
         return (np.exp(gjp)-np.exp(-1*gjp))/(np.exp(gjp)+np.exp(-1*gjp))
 
     # add nonlinear function to hjp 
     def g_j_p(self,j,p,input_data):
-        pass
+        # Now it uses sigmoid
+
+        #return self.tanh(self.h_j_p(j,p,input_data))
+        return self.sigmoid(self.h_j_p(j,p,input_data))
 
     # Jp
     def squared_error(self,p,input_data,class_data):
@@ -39,8 +49,14 @@ class BP():
 
 class file_operator():
     def __init__(self,filename):
-        pass
+        data      = np.genfromtxt(filename,delimiter=",",dtype=np.float32,usecols=(0))
+        self.data = np.c_[np.ones(len(data),dtype=np.float32),data]
+        seld.class_data = np.genfromtxt(filename,delimiter=",",dtype=np.float32,usecols=(1))
 
 
 if __name__ == "__main__":
-    pass
+    fo = file_operator(FILENAME)
+    data = fo.data
+    class_data = fo.class_data
+    bp = BP(Networks)
+    bp.learning_loop(data,class_data)
