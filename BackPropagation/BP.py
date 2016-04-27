d93d14224c7e676a6416a31b75ddff80b12ac14d
syoamakase <syoamakase@gmail.com>
2016-04-27 15:49:47 +0900

    clean up

--- BackPropagation/BP.py
+++ BackPropagation/BP.py
@@ -4,7 +4,6 @@
 import numpy as np
 
 ## to define network architecture
-# 
 Networks = {
     'num_of_layer'  : 3,
     'input_layer'   : 2,
@@ -12,12 +11,11 @@ Networks = {
     'output_layer'  : 3
 }
 
-
 ## ρ
 ROW = 0.02
 
 ## max learning epoch
-EPOCH = 500
+EPOCH = 300
 
 ## load file name
 FILENAME="class.data"
@@ -66,10 +64,11 @@ class BP():
         
         return hjp
 
-    # actiocating function - sigmoid
+    # activate function - sigmoid
     def sigmoid(self,gjp):
         return 1/(1+np.exp(-1*gjp))
 
+    # differentiate activate function(sigmoid)
     def sigmoid_dash(self,gjp):
         return gjp*(1-gjp)
 
@@ -77,6 +76,7 @@ class BP():
     def tanh(self,gjp):
         return (np.exp(gjp)-np.exp(-1*gjp))/(np.exp(gjp)+np.exp(-1*gjp))
 
+    # differentiate activate function(tanh)
     def tanh_dash(self,gjp):
         return 1-gjp**2
     
@@ -89,51 +89,16 @@ class BP():
             return self.sigmoid(self.h_j_p(l,input_data))
 
     # Jp
-    def squared_error(self,p,input_data,class_data):
-        pass
-
-    ### 'judge' may not use ###
-    def back_propagation(self,l,p,judge,class_data,gjp,epsilon_kp=None):
-        if l==self.num_of_layer-1:
-            superviser = np.zeros(3,dtype=np.int32)
-            superviser_arg = int(class_data[p]-1)
-            superviser[superviser_arg] = 1
-            epsilon_jp = (gjp[l]-superviser)*self.sigmoid_dash(gjp[l])
-            for j in range(self.net['output_layer']):
-                self.weight[l-1][j] = self.weight[l-1][j] - ROW*epsilon_jp[j]*gjp[l-1]
-            
-            self.back_propagation(l-1,p,judge,class_data,gjp,epsilon_jp)
+    def squared_error(self,p,data,class_data):
+        superviser = np.zeros(self.net['output_layer'],dtype=np.int32)
+        superviser_arg = int(class_data[p]-1)
+        superviser[superviser_arg] = 1
         
-        elif l<=0:
-            return
-
-        else:
-            dJp_dgjp = (epsilon_kp*self.weight[l].T).sum()
-            epsilon_jp = dJp_dgjp*self.sigmoid_dash(gjp[l])
-
-            #self.weight[l] = self.weight[l] - ROW*epsilpm_jp*gjp[l]
-            #if judge != class_data[p]:
-            for j in range(len(gjp[l])):
-                self.weight[l-1][j] = self.weight[l-1][j] - ROW*epsilon_jp[j]*gjp[l-1]
-
-
-            self.back_propagation(l-1,p,judge,class_data,gjp,epsilon_jp)
-
-    # to start learning
-    def learning_loop(self,input_data,class_data):
-        for epoch in range(EPOCH):
-            for p in range(len(input_data)):
-                data = input_data[p]
-                gjp_list = []
-                gjp_list.append(self.sigmoid(data))
-                for l in range(0,len(self.weight)):
-                    data = self.g_j_p(l,data)
-                    gjp_list.append(data)
-                gjp = np.array(gjp_list)
-                judge = data.argmax()
-                if judge+1 != class_data[p]:
-                    self.back_propagation(self.num_of_layer-1,p,judge,class_data,gjp)
+        squared_error = (((data - superviser)**2).sum())/2
         
+        return squared_error
+
+    def print_result(self,input_data,class_data):
         for p in range(len(input_data)):
             data = input_data[p]
             gjp_list = []
@@ -143,8 +108,58 @@ class BP():
                 gjp_list.append(data)
             gjp = np.array(gjp_list)
             judge = data.argmax()
-            print input_data[p],class_data[p]
-            print data,judge+1
+            print("input data : {},\noutput : {}".format(input_data[p],data))
+            print("\n \033[31m class : {}, prediction : {}\033[0m".format(int(class_data[p]),judge+1))
+            print("------------------------------")
+
+    # recursive function 
+    def back_propagation(self,l,p,class_data,gjp,epsilon_kp=None):
+        # deal with output layer
+        if l==self.num_of_layer-1:
+            superviser = np.zeros(self.net['output_layer'],dtype=np.int32)
+            superviser_arg = int(class_data[p]-1)
+            superviser[superviser_arg] = 1
+            epsilon_jp = (gjp[l]-superviser)*self.sigmoid_dash(gjp[l])
+            for j in range(self.net['output_layer']):
+                self.weight[l-1][j] = self.weight[l-1][j] - ROW*epsilon_jp[j]*gjp[l-1]
+            
+            # recursive call
+            self.back_propagation(l-1,p,class_data,gjp,epsilon_jp)
+        
+        # finish
+        elif l<=0:
+            return
+
+        # deal with hidden layer or input layer
+        else:
+            dJp_dgjp = (epsilon_kp*self.weight[l].T).sum()
+            epsilon_jp = dJp_dgjp*self.sigmoid_dash(gjp[l])
+
+            for j in range(len(gjp[l])):
+                self.weight[l-1][j] = self.weight[l-1][j] - ROW*epsilon_jp[j]*gjp[l-1]
+
+            # recursive call
+            self.back_propagation(l-1,p,class_data,gjp,epsilon_jp)
+
+    # to start learning
+    def learning_loop(self,input_data,class_data):
+        for epoch in range(EPOCH):
+            print('epoch : {}'.format(epoch))
+            loss = 0
+            for p in range(len(input_data)):
+                data = input_data[p]
+                gjp_list = []
+                gjp_list.append(self.sigmoid(data))
+                for l in range(0,len(self.weight)):
+                    data = self.g_j_p(l,data)
+                    gjp_list.append(data)
+                gjp = np.array(gjp_list)
+                judge = data.argmax()
+                loss += self.squared_error(p,data,class_data)
+                if judge+1 != class_data[p]:
+                    self.back_propagation(self.num_of_layer-1,p,class_data,gjp)
+            print('loss : {}'.format(loss)) 
+        self.print_result(input_data,class_data)
 
 
 class file_operator():
